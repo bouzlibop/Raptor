7745448a91dc4275f4f60af77c71914ceff96558
Adrian <bouzlibop@gmail.com>
2013-11-17 06:25:52 +0100

    express changed

--- config/express.js
+++ config/express.js
@@ -2,17 +2,16 @@ var path = require('path');
 
 module.exports = function (express, app, config, passport) {
 
-    app.set('port', process.env.PORT || 3000);
+    app.set('port', process.env.PORT || config.port);
+    app.set('env', process.env.NODE_ENV || config.env);
 
     app.set('views', path.join(config.root, 'app/views'));
     app.set('view engine', 'jade');
-//    app.set('usermodels', path.join(config.root, 'data'));
 
     app.use(express.favicon(config.root + '/public/images/favicon.ico'));
 
     app.use(express.logger('dev'));
-    //app.use(express.json()); app.use(express.urlencoded()); app.use(express.multipart());
-    app.use(express.bodyParser());
+    app.use(express.bodyParser());//app.use(express.json()); app.use(express.urlencoded()); app.use(express.multipart());
     app.use(express.methodOverride());
     app.use(express.cookieParser());
     app.use(express.session({
@@ -21,7 +20,7 @@ module.exports = function (express, app, config, passport) {
     app.use(passport.initialize());
     app.use(passport.session());
     app.use(app.router);
-    app.use(express.static(path.join(config.root,'public')));
+    app.use(express.static(path.join(config.root, 'public')));
 
     app.configure('development', function () {
         app.locals.pretty = true;
