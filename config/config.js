d77d79fe60e4c17c12d56dfa8231bbea330ad6f0
Adrian <bouzlibop@gmail.com>
2013-11-17 06:18:28 +0100

    config changed

--- config/config.js
+++ config/config.js
@@ -1,25 +1,33 @@
 var path = require('path');
-var rootPath = path.resolve(__dirname + './..');
+var rootPath = path.resolve(__dirname + '/..');
 
 module.exports = {
     production: {
         env: "production",
         root: rootPath,
-        db: process.env.MONGOHQ_URL
+        db: process.env.MONGOHQ_URL,
+        server: "http://127.0.0.1:3000",
+        port: 3000
     },
     staging: {
         env: "staging",
         root: rootPath,
-        db: process.env.MONGOHQ_URL
+        db: process.env.MONGOHQ_URL,
+        server: "http://127.0.0.1:3000",
+        port: 3000
     },
     test: {
         env: "test",
         root: rootPath,
-        db: 'mongodb://localhost/your_app_db_test'
+        db: 'mongodb://localhost/your_app_db_test',
+        server: "http://127.0.0.1:3000",
+        port: 3000
     },
     development: {
         env: "development",
         root: rootPath,
-        db: 'mongodb://localhost/test'
+        db: 'mongodb://localhost/test',
+        server: "http://127.0.0.1:3000",
+        port: 3000
     }
 }
